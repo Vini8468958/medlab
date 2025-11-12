916d568bf905a153de1b7671a8e3b80215c059af
Vinícius <vinidarthbarbosa03@gmail.com>
2025-11-11 22:27:38 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.medlab',
+  appName: 'medlab',
+  webDir: 'www'
+};
+
+export default config;
